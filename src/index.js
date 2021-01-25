084e31df5a6dd82e6eabbb623878199b4af74ff4
Rabets Vlad <vladrabets46@gmai.com>
2021-01-25 04:33:50 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let flag = true;
+    while(flag){
+        flag=false;
+      for(config of bracketsConfig){
+          let brackets = config[0]+config[1];
+          while(str.includes(brackets)){
+                  str = str.slice(0,str.indexOf(brackets)) + str.slice(str.indexOf(brackets)+brackets.length);
+                  flag=true;
+          }
+      }
+  }
+  if(str)
+      return false;
+  else
+      return true;
 }
